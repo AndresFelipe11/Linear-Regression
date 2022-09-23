ba6ec52b2f6ddccc773831e1fa41c8b6b2eddc3d
Jose Cruz <josedanielcrz@gmail.com>
2022-09-23 09:41:54 -0500

    Se termina el GUI para mostrar la gráfica de la regresión y una tabla con los datos

--- gui/GUI.py
+++ gui/GUI.py
@@ -1,5 +1,8 @@
 import tkinter as tk
+from tkinter import ttk
 from tkinter.filedialog import askopenfile
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg as figureCanvas
 from src.JsonReader import Json
 from src.regression import Regression
 
@@ -9,13 +12,17 @@ class GUI:
         self.root = tk.Tk()
         self.frameBtn = tk.Frame(self.root, bg="white")
         self.frameGraph = tk.Frame(self.root, bg="white")
+        self.canvas = None
+        self.table = None
+        self.dataToPredict = None
+        self.predictedData = None
         
         self.frameBtn.pack(side=tk.LEFT, expand=True, padx=20)
         self.frameGraph.pack(side=tk.RIGHT, expand=True)
     
     def start(self):
         self.root.title("Linear Regression")
-        self.root.geometry("750x450")
+        self.root.geometry("770x450")
         self.root.configure(bg="white")
 
         loadLabel = tk.Label(self.frameBtn, text="Select a JSON file with the data to operate", bg="white", font="Arial 10")
@@ -26,6 +33,8 @@ class GUI:
 
         graphLabel = tk.Label(self.frameGraph, text="Linear Regression Graph", font="Arial 15 bold", bg="white")
         graphLabel.pack(pady=0)
+
+        self.graph("", "", [], [], Regression([], []))
 
         self.root.mainloop()
 
@@ -42,3 +51,59 @@ class GUI:
         predictedData = regression.predict(dataToPredict)
         
         print("For {} = {} the predicted {} is {}".format(xName, dataToPredict, yName, predictedData))
+    
+        xData.append(dataToPredict)
+        yData.append(predictedData)
+
+        self.graph(xName, yName, xData, yData, regression)
+
+    def graph(self, xName, yName, xData, yData, regression):
+        self.drawTable(xName, yName, xData, yData, xData[-1] if len(xData) > 0 else 0, yData[-1] if len(yData) > 0 else 0)
+        if self.canvas is not None:
+            self.canvas.get_tk_widget().destroy()
+        
+        figure = plt.Figure(figsize=(5, 4), dpi=100)
+        ax = figure.add_subplot(111)
+        canvas = figureCanvas(figure, self.frameGraph)
+        ax.clear()
+        canvas.draw()
+        canvas.get_tk_widget().pack()
+        self.canvas = canvas
+        
+        ax.scatter(xData, yData, color='red')
+        ax.plot(xData, regression.predictList(xData), color='green')
+        ax.set_title("{} vs {}".format(xName, yName))
+        ax.set_xlabel(xName)
+        ax.set_ylabel(yName)
+
+    def drawTable(self, xName, yName, xData, yData, dataToPredict, predictedData):
+        if self.table is not None:
+            self.table.destroy()
+            self.dataToPredict.destroy()
+            self.predictedData.destroy()
+
+        table = ttk.Treeview(self.frameBtn, columns=(xName, yName), show="headings")
+        table.column(xName, width=100, anchor="center")
+        table.column(yName, width=100, anchor="center")
+        table.heading(xName, text=xName)
+        table.heading(yName, text=yName)
+
+        for i in range(len(xData) - 1):
+            table.insert("", tk.END, values=(xData[i], yData[i]))
+
+        dataToPredictLabel = tk.Text(self.frameBtn, height=1, font="Arial 10", bg="white", width=20, borderwidth=0)
+        dataToPredictLabel.tag_configure("bold", font="Arial 10 bold")
+        dataToPredictLabel.insert(tk.END, "Data to predict: ", "bold")
+        dataToPredictLabel.insert(tk.END, dataToPredict)
+
+        predictedDataLabel = tk.Text(self.frameBtn, height=1, font="Arial 10", bg="white", width=20, borderwidth=0)
+        predictedDataLabel.tag_configure("bold", font="Arial 10 bold")
+        predictedDataLabel.insert(tk.END, "Predicted data: ", "bold")
+        predictedDataLabel.insert(tk.END, predictedData)
+        
+        self.table = table
+        self.dataToPredict = dataToPredictLabel
+        self.predictedData = predictedDataLabel
+        table.pack(pady=30)
+        dataToPredictLabel.pack(pady=10)
+        predictedDataLabel.pack(pady=10)
